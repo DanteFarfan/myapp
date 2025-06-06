983d26d0e7ed3b8352d7008fb0e0d100bca4ccfa
InFin311 <130001695+InFin311@users.noreply.github.com>
2025-06-05 23:42:33 -0400

    bug

--- lib/view/usuario_screen.dart
+++ lib/view/usuario_screen.dart
@@ -133,8 +133,12 @@ class UsuarioScreen extends StatelessWidget {
       ),
       body: Center(
 <<<<<<< HEAD
+<<<<<<< HEAD
 <<<<<<< Updated upstream
         child: SingleChildScrollView(
+=======
+        child: Padding(
+>>>>>>> parent of 0e92e39 (eliminar y editar cuenta)
 =======
         child: Padding(
 >>>>>>> parent of 0e92e39 (eliminar y editar cuenta)
